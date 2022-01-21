949f45257a84ceaa33192f409abd4a65c7117964
jamesonmcg <jameson.mcghee1@gmail.com>
2022-01-21 16:43:53 -0500

    Updated Drone and Sonobuoy files for e2e-test CI Pipeline
    * Updated the Drone file to include the e2e-tests
    * Added the kustomization file from the Opni repo to allow for use of the insights service and the k3d log adapter
    * Updated the Sonobuoy script to notify test results and build failure when tests fail

--- opni-insights-service/tests/sonobuoy/shell_scripts/sonobuoy_run.sh
+++ opni-insights-service/tests/sonobuoy/shell_scripts/sonobuoy_run.sh
@@ -2,6 +2,7 @@
 # sonobuoy_run.sh
 # assume Opni and log adapters are already installed
 
+set -e
 sonobuoy run \
 --kubeconfig ~/.kube/config \
 --namespace "opni-sono" \
@@ -9,6 +10,15 @@ sonobuoy run \
 sleep 2
 for i in {1..5}; do sonobuoy logs -n opni-sono && break || sleep .5; done
 for i in {1..60}; do sonobuoy retrieve -n opni-sono && break || sleep 5; done
-sonobuoy status -n opni-sono
+sonobuoy_status=$(sonobuoy status -n opni-sono --json | jq -r '.plugins[] | select(.plugin=="opni-sonobuoy") | ."result-status"')
+if [ "$sonobuoy_status" != "passed" ]; then
+  sonobuoy logs -n opni-sono
+  echo "SONOBUOY TESTS FAILED! View report for more details"
+  exit 1
+fi
+if [ "$sonobuoy_status" == "passed" ]; then
+  echo "SONOBUOY TESTS PASSED!"
+  exit 1
+fi
 sonobuoy delete --wait
-kubectl delete namespace opni-sono
+kubectl delete ns opni-sono --wait=false
